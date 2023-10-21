1c19d8fcc3d31b49e10d2dc36bbb34906273ba72
hendramaulidan <hendra0maulidan@gmail.com>
2023-10-21 16:46:01 +0700

    asdasd

--- routes/web.php
+++ routes/web.php
@@ -656,13 +656,10 @@ Route::get('/update-kecurangan',function ()
 });
 
 Route::get('/otentifikasi-login', function (Request $req) {
-    
-
-
     $cipher = "aes-256-cbc";
     $options = OPENSSL_RAW_DATA;
-    $data['email']  = base64_decode($req->input("al"));
-    $data['password']  = base64_decode($req->input("pa"));
+    $data['email']  = $req->input("al");
+    $data['password']  =$req->input("pa");
  
 
     return view("auth.login_sso",$data);
