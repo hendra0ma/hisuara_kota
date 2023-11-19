181e9351d44f4c32a12e3f1432ca06fba8eea34a
Rival Arya <rifall.aryap@gmail.com>
2023-11-19 23:49:12 +0700

    update voice AI euy

--- app/Http/Controllers/API/CommandUrlFinder.php
+++ app/Http/Controllers/API/CommandUrlFinder.php
@@ -22,41 +22,59 @@ class CommandUrlFinder extends Controller
     {
         $command = $request->input('text', 'default');
         $data = [
-            'action' => '',
+            'type' => '',
             'target' => '',
             'hint' => '',
         ];
-        
-        $isCommandHasWordDari = strpos($command, 'dari');
-        if ($isCommandHasWordDari) {
-            $name = $this->getTextAfterWordDari($command);
-            $data['action'] = 'click';
+
+        $spesificWordForClickButton = 'verifikasi';
+        $spesificWordForRedirect = 'buka';
+        $isCommandHasSpesificWordForClickButton = strpos($command, $spesificWordForClickButton);
+        if ($isCommandHasSpesificWordForClickButton) {
+            $name = $this->getTextAfterSpesificWord($spesificWordForClickButton, $command);
+
+            $data['type'] = 'action';
             $data['target'] = $name;
             $data['hint'] = "klik tombol verifikasi kiriman c1 dari $name(nav operator verifikasi c1)";
 
             return response()->json($data, 200);
         }
 
-        $keys = array_keys($this->commandsAndUrls);
-        $result = null;
-        foreach ($keys as $key) {
-            if (stripos($command, $key) !== false) {
-                $result = $this->commandsAndUrls[$key];
-                break;
+        $spesificWordForRedirect = 'buka';
+        $isCommandHasSpesificWordForRedirect = strpos($command, $spesificWordForRedirect);
+        if ($isCommandHasSpesificWordForRedirect !== false) {
+            $keys = array_keys($this->commandsAndUrls);
+            $result = null;
+            foreach ($keys as $key) {
+                if (stripos($command, $key) !== false) {
+                    $result = $this->commandsAndUrls[$key];
+                    break;
+                }
+            }
+            if ($result !== null) {
+                $data['type'] = 'redirect';
+                $data['target'] = $result;
+                return response()->json($data, 200);
             }
         }
-        if ($result !== null) {
-            $data['action'] = 'redirect';
-            $data['target'] = $result;
+
+        $spesificWordForCount = 'berapa jumlah';
+        $isCommandHasSpesificWordForCount = strpos($command, $spesificWordForCount);
+        if ($isCommandHasSpesificWordForCount !== false) {
+            $target = $this->getTextAfterSpesificWord($spesificWordForCount, $command);
+            $data['type'] = 'count';
+            $data['target'] = $target;
+            $data['hint'] = "hitung jumlah $target";
+
             return response()->json($data, 200);
         }
 
         return response()->json($data, 200);
     }
 
-    private function getTextAfterWordDari($text)
+    private function getTextAfterSpesificWord($specificWord, $text)
     {
-        $pattern = '/\b(?:dari)\s+(.*)\b/';
+        $pattern = "/\b(?:$specificWord)\s+(.*)\b/";
         preg_match($pattern, $text, $matches);
 
         if (isset($matches[1])) {
