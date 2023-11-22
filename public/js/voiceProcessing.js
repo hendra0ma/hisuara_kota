b20a50f35a56af10b49197c1eaa74b8209f4069c
Rival Arya <rifall.aryap@gmail.com>
2023-11-22 17:34:25 +0700

    pindah ke js

--- public/js/voiceProcessing.js
+++ public/js/voiceProcessing.js
@@ -218,11 +218,11 @@ try {
           }
 
           if (isCommandHasKeywordScrollUp) {
-            window.scrollBy(0, -1000);
+            window.scrollBy(0, -800);
           }
 
           if (isCommandHasKeywordScrollDown) {
-            window.scrollBy(0, 1000);
+            window.scrollBy(0, 800);
           }
 
         }
