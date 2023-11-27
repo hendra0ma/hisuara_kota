a2183c1d72278f52cb44c88a91e94aa902ea553a
isa-ardi <54489663+isa-ardi@users.noreply.github.com>
2023-11-27 22:19:43 +0700

    provinsi

--- resources/views/provinsi/index.blade.php
+++ resources/views/provinsi/index.blade.php
@@ -134,8 +134,9 @@
                                                 <div class="row">
                                                     <div class="col-12">
                                                         <div class="mx-auto counter-icon box-shadow-secondary brround candidate-name text-white "
-                                                            style="margin-bottom: 0; background-color: {{ $pas->color }};">
-                                                            {{ $i }}
+                                                            style="margin-bottom: 0; background-color: {{ $pas->color }};position:relative; overflow: hidden">
+                                                            <img style="bottom: -10px; position: absolute; left: 50%; transform: translateX(-50%);"
+                                                                src="{{ asset('') }}storage/{{ $pas->picture }}" alt="">
                                                         </div>
                                                     </div>
                                                     <div class="col text-center">
