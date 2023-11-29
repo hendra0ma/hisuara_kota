b8f62eac37745c9a4836ec58a19ed57163f91837
isa-ardi <54489663+isa-ardi@users.noreply.github.com>
2023-11-29 13:32:26 +0700

    remove kota from pusat

--- resources/views/pusat/index.blade.php
+++ resources/views/pusat/index.blade.php
@@ -193,7 +193,7 @@
                             <div class="text-center title-atas-table fs-5 mb-0 fw-bold">Hasil Perhitungan Suara</div>
                             <div class="text-center title-atas-table fs-5 mb-0 fw-bold">Pemilihan Presiden dan Wakil
                                 Presiden</div>
-                            <div class="text-center title-atas-table fs-5 fw-bold">{{ $kota['name'] }}</div>
+                            <div class="text-center title-atas-table fs-5 fw-bold">Tingkat Nasional</div>
                             <div class="row mx-auto" style="width: 884.5px;">
 
                             </div>
