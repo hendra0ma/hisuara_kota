3c2c6a2c0f14fc5eb851f543db7b71d3ad718511
hendra0ma <53743349+hendra0ma@users.noreply.github.com>
2023-12-01 14:01:26 +0700

    aku tau tuh

--- app/Http/Controllers/VerificatorController.php
+++ app/Http/Controllers/VerificatorController.php
@@ -56,31 +56,35 @@ class VerificatorController extends Controller
      public $configs;
      public function __construct()
      {
- 
          $currentDomain = request()->getHttpHost();
-         $url = substr($currentDomain, 0, strpos($currentDomain, ':8000'));
-         $regency_id = RegenciesDomain::where('domain',"LIKE","%".$url."%")->first();
+         if (isset(parse_url($currentDomain)['port'])) {
+             $url = substr($currentDomain, 0, strpos($currentDomain, ':8000'));
+         } else {
+             $url = $currentDomain;
+         }
+         $regency_id = RegenciesDomain::where('domain', 'LIKE', '%' . $url . '%')->first();
  
          $this->configs = Config::first();
-         $this->config = new Configs;
-         $this->config->regencies_id =  (string) $regency_id->regency_id;
-         $this->config->provinces_id =  $this->configs->provinces_id;
-         $this->config->setup =  $this->configs->setup;
-         $this->config->updated_at =  $this->configs->updated_at;
-         $this->config->created_at =  $this->configs->created_at;
-         $this->config->partai_logo =  $this->configs->partai_logo;
-         $this->config->date_overlimit =  $this->configs->date_overlimit;
-         $this->config->show_public =  $this->configs->show_public;
-         $this->config->show_terverifikasi =  $this->configs->show_terverifikasi;
-         $this->config->lockdown =  $this->configs->lockdown;
-         $this->config->multi_admin =  $this->configs->multi_admin;
-         $this->config->otonom =  $this->configs->otonom;
-         $this->config->dark_mode =  $this->configs->dark_mode;
-         $this->config->jumlah_multi_admin =  $this->configs->jumlah_multi_admin;
-         $this->config->jenis_pemilu =  $this->configs->jenis_pemilu;
-         $this->config->tahun =  $this->configs->tahun;
-         $this->config->quick_count =  $this->configs->quick_count;
-         $this->config->default =  $this->configs->default;
+         $this->config = new Configs();
+         $this->config->regencies_id = (string) $regency_id->regency_id;
+         $this->config->provinces_id = $this->configs->provinces_id;
+         $this->config->setup = $this->configs->setup;
+         $this->config->updated_at = $this->configs->updated_at;
+         $this->config->created_at = $this->configs->created_at;
+         $this->config->partai_logo = $this->configs->partai_logo;
+         $this->config->date_overlimit = $this->configs->date_overlimit;
+         $this->config->show_public = $this->configs->show_public;
+         $this->config->show_terverifikasi = $this->configs->show_terverifikasi;
+         $this->config->lockdown = $this->configs->lockdown;
+         $this->config->multi_admin = $this->configs->multi_admin;
+         $this->config->otonom = $this->configs->otonom;
+         $this->config->dark_mode = $this->configs->dark_mode;
+         $this->config->jumlah_multi_admin = $this->configs->jumlah_multi_admin;
+         $this->config->jenis_pemilu = $this->configs->jenis_pemilu;
+         $this->config->tahun = $this->configs->tahun;
+         $this->config->quick_count = $this->configs->quick_count;
+         $this->config->default = $this->configs->default;
+     }$this->config->default =  $this->configs->default;
      }
 
     public function index()
@@ -521,8 +525,6 @@ class VerificatorController extends Controller
         // $data['jumlah_tps_terverifikai'] = Tps::join('saksi', 'saksi.tps_id', '=', 'tps.id')->where('saksi.verification', 1)->count();
         $data['total_tps']   =  Tps::where('setup','belum terisi')->count();
         // $data['jumlah_kosong']  =  $data['total_tps'] - $data['jumlah_tps_masuk'];
-
-        return  $this->config;
         return view('administrator.c1.verifikasi-c1', $data);
     }
 
